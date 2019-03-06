9c32e3668954b7c5c87633507a298d159e09dfb4
giumas <giumas@yahoo.it>
2019-03-05 21:18:30 -0500

    added test for Format

---
+++ tests/lib/sources/test_format.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+
+from hyo2.abc.lib.testing import Testing
+from hyo2.openbst.app import app_info  # for GDAL data
+from hyo2.openbst.lib.sources.bag import Format, FormatType
+
+
+class FakeFormat(Format):
+
+    def read_data_types(self, data_types: list) -> dict:
+        return dict()
+
+    def save_data_types(self, data_layers: dict) -> bool:
+        return False
+
+
+class TestLibFormatType(unittest.TestCase):
+
+    def test_all(self):
+        for fmt_type in FormatType:
+            self.assertGreaterEqual(fmt_type.value, 0)
+
+
+class TestLibFormat(unittest.TestCase):
+
+    def setUp(self):
+        self.testing = Testing(
+            root_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)))
+
+    def test_read(self):
+
+        for input_path in self.testing.input_test_files(ext=".tif"):
+            fmt = FakeFormat(path=input_path)
+            success = fmt.retrieve_spatial_info_with_gdal()
+            self.assertTrue(success)
+
+
+def suite():
+    s = unittest.TestSuite()
+    s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLibFormat))
+    return s
