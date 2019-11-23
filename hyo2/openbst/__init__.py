89facbafb26def042fa4108c88f968f058e1a23a
giumas <giuseppemasetti@gmail.com>
2019-11-23 12:17:48 -0500

    version updated

--- hyo2/openbst/__init__.py
+++ hyo2/openbst/__init__.py
@@ -3,8 +3,8 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 name = "OpenBST"
-__version__ = "0.1.1"
-__author__ = "gmasetti@ccom.unh.edu, jean.marie.augustin@ifremer.fr, cyrille.poncelet@ifremer.fr"
+__version__ = "0.2.0"
+__author__ = "gmasetti@ccom.unh.edu, msmith@ccom.unh.edu, jean.marie.augustin@ifremer.fr, cyrille.poncelet@ifremer.fr"
 __license__ = "LGPLv3 license"
 __copyright__ = "Copyright 2019 University of New Hampshire, Center for Coastal and Ocean Mapping; IFREMER. " \
                 "All rights reserved. Portions of this project were developed under a cooperative agreement " \
