46daf43048d985afb9a5fcdd7b2095287aeca964
Michael <msmith@ccom.unh.edu>
2020-01-10 15:10:08 -0500

    Process Management (Continued)
    Removal of Process management code from process file

--- hyo2/openbst/lib/processing/process.py
+++ hyo2/openbst/lib/processing/process.py
@@ -8,19 +8,11 @@ from pathlib import Path
 
 from hyo2.openbst.lib.nc_helper import NetCDFHelper
 from hyo2.openbst.lib.processing.parameters import Parameters
+from hyo2.openbst.lib.processing.process_management.process_manager import ProcessManager
 from hyo2.openbst.lib.processing.raw_decoding import RawDecoding
+from hyo2.openbst.lib.processing.static_gain_compensation import StaticGainCorrection
 
 logger = logging.getLogger(__name__)
-
-
-class ProcessStageStatus(Enum):
-    # Already in processing chain
-    PRIORPROCESS = 0
-    REPEATEPROCESS = 1
-
-    # Not in processing chain
-    NEWPROCESS = 2
-    MODIFIEDPROCESS = 3
 
 
 class Process:
@@ -28,28 +20,12 @@ class Process:
 
     def __init__(self, process_path: Path) -> None:
         self._path = process_path
-        self._step = 00
-        self._prior_process = ''
+        self.proc_manager = ProcessManager()
+
 
     @property
     def path(self) -> Path:
         return self._path
-
-    @property
-    def step(self) -> int:
-        return self._step
-
-    @step.setter
-    def step(self, step):
-        self._step = step
-
-    @property
-    def parent_process(self) -> str:
-        return self._prior_process
-
-    @parent_process.setter
-    def parent_process(self, current_process):
-        self._prior_process = current_process
 
     @property
     def raw_process_list(self) -> list:
@@ -84,71 +60,26 @@ class Process:
             return True
 
     def store_process(self, nc: Dataset, process_string: str) -> bool:
-
         NetCDFHelper.update_modified(ds=nc)
-
         nc.close()
 
-        if process_string.split('__')[0] == '00':
-            self.step = 0
-        else:
-            self.step += 1
-
-        self.parent_process = process_string
+        # if process_string.split('__')[0] == '00':
+        #     self.step = 0
+        # else:
+        #     self.step += 1
+        # self.parent_process = process_string
         return True
-
-    def check_process(self, nc_process: Dataset, process_string: str) -> ProcessStageStatus:
-        process_identifiers = process_string.split('__')
-        parent_identifiers = self.parent_process.split('__')
-
-        # Check if this is the first process
-        if self.parent_process == '':
-            return ProcessStageStatus.NEWPROCESS
-
-        # Check new process against parent process
-        if process_identifiers[0] == parent_identifiers[1]:
-            if process_identifiers[-1] == parent_identifiers[-1]:
-                return ProcessStageStatus.PRIORPROCESS
-            else:
-                return ProcessStageStatus.MODIFIEDPROCESS
-        else:
-            # Check if step has been computed prior in chain
-            in_process_chain = self.check_process_chain(nc_process=nc_process,
-                                                        process_str=process_string,
-                                                        parent_str=self.parent_process)
-
-    @staticmethod
-    def check_process_chain(nc_process: Dataset, process_str: str, parent_str: str) -> bool:
-        process_identifiers = process_str.split('__')
-        # Find grp_process that matches current parent
-        grp_prior = nc_process.groups[parent_str]
-        nc_parent_str = grp_prior.parent_process
-        nc_parent_identifiers = nc_parent_str.split('__')
-        if nc_parent_identifiers[1] == '':
-            return False
-        elif nc_parent_identifiers[1] == process_identifiers[1]:
-            return True
-        else:
-            Process.check_process_chain(nc_process=nc_process, process_str=process_str, parent_str=nc_parent_str)
-
-
-        # Grab parent
-
-        # Compare parents
-
-        # if not same recursive call
 
     # ## Processing Methods ##
     def raw_decode(self, process_file_path: Path, raw_path: Path, parameters: Parameters):
         # create nc objects
         ds_process = Dataset(filename=process_file_path, mode='a')
         ds_raw = Dataset(filename=raw_path, mode='a')
-
         params_raw_decode = parameters.rawdecode
 
-        # check process has not been calculated before
-        process_name_str = params_raw_decode.process_hash()
-        has_been_processd = self.check_process(nc_process=ds_process, process_string=process_name_str)
+        # check process chain
+        
+        has_been_processd = self.proc_manager.(nc_process=ds_process, process_hash=params_raw_decode.process_hash())
 
         if has_been_processd == ProcessStageStatus.PRIORPROCESS:
             logger.debug("Processing step has been computed prior")
