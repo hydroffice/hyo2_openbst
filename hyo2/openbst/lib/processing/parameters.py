6ea758bbba4736b787a0f26df2a18f1016c0f19e
Michael <msmith@ccom.unh.edu>
2020-01-11 14:11:15 -0500

    //WIP - parameters sub object selection
    added a method to select the appropriate method parameter object based on the desired processing type

--- hyo2/openbst/lib/processing/parameters.py
+++ hyo2/openbst/lib/processing/parameters.py
@@ -1,5 +1,6 @@
 import logging
 
+from hyo2.openbst.lib.processing.process_types.dicts import ProcessTypes
 from hyo2.openbst.lib.processing.process_types.raw_decoding import RawDecodeParameters
 from hyo2.openbst.lib.processing.process_types.static_gain_compensation import StaticGainParameters
 
@@ -12,3 +13,10 @@ class Parameters:
     def __init__(self):
         self.rawdecode = RawDecodeParameters()
         self.static_gains = StaticGainParameters()
+
+    # TODO: Can there be a check here to create object if not yet created
+    def get_process_params(self, process_type: ProcessTypes):
+        if process_type == ProcessTypes.RAWDECODE:
+            return self.rawdecode
+        elif process_type == ProcessTypes.STATICGAIN:
+            return self.static_gains
