abd7fa0ffe35b1ce51cad0f45652c1af734b236a
Michael <zxcvbnmike15@gmail.com>
2020-01-15 12:14:43 -0500

    Raw_decode Refactor
    modified the process method types to explicitly accept input parameters. This decouples the process method types from the nc files for standalone use

--- hyo2/openbst/lib/processing/process_methods/raw_decoding.py
+++ hyo2/openbst/lib/processing/process_methods/raw_decoding.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+from typing import Optional
 
 from enum import Enum
 from netCDF4 import Dataset, Group
@@ -11,19 +12,12 @@ logger = logging.getLogger(__name__)
 
 # ## Raw Decode Enum and Dictionaries ##
 class RawDecodeEnum(Enum):
-
     perbeam_from_snippets_using_median = 0
     perbeam_from_snippets_using_mean = 1
     perbeam_from_sonar_beam_average = 2
 
 
-raw_decod_enum = {
-    RawDecodeEnum.perbeam_from_snippets_using_median: 'perbeam_from_snippets_using_median',
-    RawDecodeEnum.perbeam_from_snippets_using_mean: 'perbeam_from_snippets_using_mean',
-    RawDecodeEnum.perbeam_from_sonar_beam_average: 'perbeam_from_sonar_beam_averages'
-}
-
-raw_decode_plot_title = {
+raw_decode_title = {
     RawDecodeEnum.perbeam_from_snippets_using_median: "Raw decoded backscatter - from snippets using median ",
     RawDecodeEnum.perbeam_from_snippets_using_mean: "Raw decoded backscatter - from snippets using mean",
     RawDecodeEnum.perbeam_from_sonar_beam_average: "Raw decoded backscatter - Sonar provided beam averages"
@@ -72,24 +66,59 @@ class RawDecoding:
         p_use_window = parameters.use_window
         p_sample_window_size = parameters.sample_window_size
 
+        grp_snippets = ds_raw.groups['snippets']
+        var_snippets = grp_snippets.variables['snippets']
+        data_snippets = var_snippets[:]
+        data_beam_mask = ~data_snippets.mask[:, :, 0]
+
         if p_method_type is RawDecodeEnum.perbeam_from_snippets_using_mean:
-            grp_snippets = ds_raw.groups['snippets']
-            bs_raw_decode = RawDecoding.perbeam_bs_from_snippits_using_mean(grp_snippets=grp_snippets,
-                                                                            use_window=p_use_window,
-                                                                            window_size=p_sample_window_size)
+            if p_use_window is False:
+                bs_raw_decode = cls.perbeam_bs_from_snippits_using_mean(snippet_samples=data_snippets,
+                                                                        beam_mask=data_beam_mask,
+                                                                        use_window=False)
+            else:
+                var_detection_sample = grp_snippets.variables['detect_sample']
+                data_dection_samples = var_detection_sample[:]
+                var_snippets_start_sample = grp_snippets.variables['snippet_start_sample']
+                data_snippets_start_sample = var_snippets_start_sample[:]
+                var_snippets_end_sample = grp_snippets.variables['snippet_end_sample']
+                data_snippets_end_sample = var_snippets_end_sample
+
+                bs_raw_decode = cls.perbeam_bs_from_snippits_using_mean(snippet_samples=data_snippets,
+                                                                        beam_mask=data_beam_mask,
+                                                                        ping_detect_samples=data_dection_samples,
+                                                                        ping_snippet_start=data_snippets_start_sample,
+                                                                        ping_snippet_end=data_snippets_end_sample,
+                                                                        use_window=p_use_window,
+                                                                        window_size=p_sample_window_size)
 
         elif p_method_type is RawDecodeEnum.perbeam_from_snippets_using_median:
-            grp_snippets = ds_raw.groups['snippets']
-            bs_raw_decode = RawDecoding.perbeam_bs_from_snippets_using_median(grp_snippets=grp_snippets,
-                                                                              use_window=p_use_window,
-                                                                              window_size=p_sample_window_size)
+            if p_use_window is False:
+                bs_raw_decode = cls.perbeam_bs_from_snippets_using_median(snippet_samples=data_snippets,
+                                                                          beam_mask=data_beam_mask,
+                                                                          use_window=False)
+            else:
+                var_detection_sample = grp_snippets.variables['detect_sample']
+                data_dection_samples = var_detection_sample[:]
+                var_snippets_start_sample = grp_snippets.variables['snippet_start_sample']
+                data_snippets_start_sample = var_snippets_start_sample[:]
+                var_snippets_end_sample = grp_snippets.variables['snippet_end_sample']
+                data_snippets_end_sample = var_snippets_end_sample
+
+                bs_raw_decode = cls.perbeam_bs_from_snippets_using_median(snippet_samples=data_snippets,
+                                                                          beam_mask=data_beam_mask,
+                                                                          ping_detect_samples=data_dection_samples,
+                                                                          ping_snippet_start=data_snippets_start_sample,
+                                                                          ping_snippet_end=data_snippets_end_sample,
+                                                                          use_window=p_use_window,
+                                                                          window_size=p_sample_window_size)
 
         elif p_method_type is RawDecodeEnum.perbeam_from_sonar_beam_average:
             grp_raw_bathy = ds_raw.groups['raw_bathymetry_data']
             bs_raw_decode = RawDecoding.perbeam_bs_from_beam_average(raw_bathy=grp_raw_bathy)
 
         else:
-            raise TypeError("Unrecognized Raw Decode Method: %s")   # TODO: Fix the error message
+            raise TypeError("Unrecognized Raw Decode Method: %s")  # TODO: Fix the error message
 
         data_out = {
             'backscatter_data': bs_raw_decode
@@ -112,109 +141,96 @@ class RawDecoding:
         except RuntimeError:
             return False
 
-    # ## Processing Method Types ##
+    # # Processing Method Types #
     @classmethod
-    def perbeam_bs_from_snippets_using_median(cls, grp_snippets: Group, use_window=False, window_size=5) -> np.ndarray:
-        var_snippets = grp_snippets.variables['snippets']
-        snippet_samples = var_snippets[:]
-        beam_mask = ~snippet_samples.mask[:, :, 0]
-
+    def perbeam_bs_from_snippets_using_median(cls,
+                                              snippet_samples: np.ma.MaskedArray,
+                                              beam_mask: np.ma.MaskedArray,
+                                              ping_detect_samples: Optional[np.ma.MaskedArray] = None,
+                                              ping_snippet_start: Optional[np.ma.MaskedArray] = None,
+                                              ping_snippet_end: Optional[np.ma.MaskedArray] = None,
+                                              use_window: bool = False, window_size: int = 5) -> np.ndarray:
         if use_window is False:
-            perbeam_bs = 20 * np.ma.log10(np.nanmedian(snippet_samples, axis=2))
+            perbeam_bs = 20 * np.ma.log10(np.ma.median(snippet_samples, axis=2))
         elif use_window is True:
-            # Determine how many samples to grab around the center detection
-            if window_size & 1:  # Odd
-                start_index_identifier = (window_size - 1) / 2
-                end_index_identifier = (window_size - 1) / 2
-            else:  # Even
-                start_index_identifier = window_size / 2
-                end_index_identifier = window_size / 2 - 1
-
-            var_snippet_start_sample = grp_snippets.variables['snippet_start_sample']
-            var_snippet_end_sample = grp_snippets.variables['snippet_end_sample']
-            var_snippet_sample_len = grp_snippets.variables['snippet_samples_length']
-            var_snippet_detect_sample = grp_snippets.variables['detect_sample']
+            # Check that all needed data is present and set up the window
+            window_start_id, window_end_id = RawDecoding.setup_window(window_size=window_size,
+                                                                      detect_samples=ping_detect_samples,
+                                                                      snippet_start_samples=ping_snippet_start,
+                                                                      snippet_end_samples=ping_snippet_end)
 
             num_pings = snippet_samples.shape[0]
             num_beams = snippet_samples.shape[1]
-
-            snippet_end_sample = var_snippet_end_sample[:] - var_snippet_start_sample[:]
-            snippet_detect_sample = var_snippet_detect_sample[:] - var_snippet_start_sample[:]
-            snippet_sample_len = var_snippet_sample_len[:]
 
             perbeam_bs = np.ones(shape=(num_pings, num_beams,)) * np.nan
             for ping in range(num_pings):
                 for beam in range(num_beams):
                     if beam_mask[ping, beam]:
                         samples = snippet_samples[ping, beam, :]
+                        detection_sample_index = ping_detect_samples[ping, beam] - ping_snippet_start[ping, beam]
+                        end_sample_index = ping_snippet_end[ping, beam] - ping_snippet_start[ping, beam]
+                        number_of_snippets = end_sample_index + 1
 
-                        if snippet_sample_len[ping, beam] <= window_size:
-                            perbeam_bs[ping, beam] = np.nanmean(samples)
+                        if number_of_snippets <= window_size:
+                            perbeam_bs = np.ma.mean(samples)
                         else:
-                            start_index = int(snippet_detect_sample[ping, beam] - start_index_identifier)
-                            end_index = int(snippet_detect_sample[ping, beam] + end_index_identifier)
+                            start_index = int(detection_sample_index - window_start_id)
+                            end_index = int(detection_sample_index + window_end_id)
 
                             if start_index < 0:
                                 start_index = 0
-                            if end_index > snippet_end_sample[ping, beam]:
-                                end_index = int(snippet_end_sample[ping, beam])
+                            if end_index > end_sample_index:
+                                end_index = int(end_sample_index)
 
                             perbeam_bs[ping, beam] = np.median(samples[start_index:end_index + 1])
-            perbeam_bs = 20*np.log10(perbeam_bs)
+            perbeam_bs = 20 * np.log10(perbeam_bs)
         else:
             raise TypeError("'use_window' must be of type 'bool': %s" % type(use_window))
 
         return perbeam_bs
 
     @classmethod
-    def perbeam_bs_from_snippits_using_mean(cls, grp_snippets: Group, use_window=False, window_size=None) -> np.ndarray:
-        var_snippets = grp_snippets.variables['snippets']
-        snippet_samples = var_snippets[:]
-        beam_mask = ~snippet_samples.mask[:, :, 0]
-
+    def perbeam_bs_from_snippits_using_mean(cls,
+                                            snippet_samples: np.ma.MaskedArray,
+                                            beam_mask: np.ma.MaskedArray,
+                                            ping_detect_samples: Optional[np.ma.MaskedArray] = None,
+                                            ping_snippet_start: Optional[np.ma.MaskedArray] = None,
+                                            ping_snippet_end: Optional[np.ma.MaskedArray] = None,
+                                            use_window: bool = False, window_size: int = 5) -> np.ndarray:
         if use_window is False:
-            perbeam_bs = 20 * np.ma.log10(np.mean(snippet_samples, axis=2))
-
+            perbeam_bs = 20 * np.ma.log10(np.ma.mean(snippet_samples, axis=2))
         elif use_window is True:
-            # Determine how many samples to grab around the center detection
-            if window_size & 1:                             # Odd
-                start_index_identifier = (window_size-1)/2
-                end_index_identifier = (window_size-1)/2
-            else:                                           # Even
-                start_index_identifier = window_size/2
-                end_index_identifier = window_size/2 - 1
-
-            var_snippet_start_sample = grp_snippets.variables['snippet_start_sample']
-            var_snippet_end_sample = grp_snippets.variables['snippet_end_sample']
-            var_snippet_sample_len = grp_snippets.variables['snippet_samples_length']
-            var_snippet_detect_sample = grp_snippets.variables['detect_sample']
+            # Check that all needed data is present and set up the window
+            window_start_id, window_end_id = RawDecoding.setup_window(window_size=window_size,
+                                                                      detect_samples=ping_detect_samples,
+                                                                      snippet_start_samples=ping_snippet_start,
+                                                                      snippet_end_samples=ping_snippet_end)
 
             num_pings = snippet_samples.shape[0]
             num_beams = snippet_samples.shape[1]
 
-            snippet_end_sample = var_snippet_end_sample[:] - var_snippet_start_sample[:]
-            snippet_detect_sample = var_snippet_detect_sample[:] - var_snippet_start_sample[:]
-            snippet_sample_len = var_snippet_sample_len[:]
-
-            perbeam_bs = np.ones(shape=(num_pings, num_beams, )) * np.nan
+            perbeam_bs = np.ones(shape=(num_pings, num_beams,)) * np.nan
             for ping in range(num_pings):
                 for beam in range(num_beams):
                     if beam_mask[ping, beam]:
                         samples = snippet_samples[ping, beam, :]
+                        detection_sample_index = ping_detect_samples[ping, beam] - ping_snippet_start[ping, beam]
+                        end_sample_index = ping_snippet_end[ping, beam] - ping_snippet_start[ping, beam]
+                        number_of_snippets = end_sample_index + 1
 
-                        if snippet_sample_len[ping, beam] <= window_size:
-                            perbeam_bs[ping, beam] = np.mean(samples)
+                        if number_of_snippets <= window_size:
+                            perbeam_bs = np.ma.mean(samples)
                         else:
-                            start_index = int(snippet_detect_sample[ping, beam] - start_index_identifier)
-                            end_index = int(snippet_detect_sample[ping, beam] + end_index_identifier)
+                            start_index = int(detection_sample_index - window_start_id)
+                            end_index = int(detection_sample_index + window_end_id)
 
                             if start_index < 0:
                                 start_index = 0
-                            if end_index > snippet_end_sample[ping, beam]:
-                                end_index = int(snippet_end_sample[ping, beam])
+                            if end_index > end_sample_index:
+                                end_index = int(end_sample_index)
 
-                            perbeam_bs[ping, beam] = np.mean(samples[start_index:end_index+1])
-            perbeam_bs = 20*np.log10(perbeam_bs)
+                            perbeam_bs[ping, beam] = np.mean(samples[start_index:end_index + 1])
+            perbeam_bs = 20 * np.log10(perbeam_bs)
 
         else:
             raise TypeError("'use_window' must be of type 'bool': %s" % type(use_window))
@@ -225,4 +241,29 @@ class RawDecoding:
     def perbeam_bs_from_beam_average(cls, raw_bathy: Group) -> np.ndarray:
         var_bs_beam_averages = raw_bathy.variables['bs_beam_average']
         perbeam_bs = var_bs_beam_averages[:]
-        return 20*np.ma.log10(perbeam_bs)
+        return 20 * np.ma.log10(perbeam_bs)
+
+    @staticmethod
+    def setup_window(window_size: int, detect_samples: Optional[np.ma.MaskedArray],
+                     snippet_start_samples: Optional[np.ma.MaskedArray],
+                     snippet_end_samples: Optional[np.ma.MaskedArray]):
+
+        # Check that all needed data is present
+        data_list = [snippet_start_samples, snippet_end_samples, detect_samples]
+        missing_data = list()
+        for data in data_list:
+            if data is None:
+                missing_data.append(True)
+
+        if any(missing_data) is True:
+            error_string = "Parameters indicate use_window=True, but required data is missing.:"
+            raise RuntimeError(error_string)
+
+        # Determine how many samples to grab around the center detection
+        if window_size & 1:  # Odd
+            start_index_identifier = (window_size - 1) / 2
+            end_index_identifier = (window_size - 1) / 2
+        else:  # Even
+            start_index_identifier = window_size / 2
+            end_index_identifier = window_size / 2 - 1
+        return start_index_identifier, end_index_identifier
