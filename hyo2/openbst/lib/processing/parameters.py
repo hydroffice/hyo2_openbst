03beee4621a1de0cf3de35114ffa2d7a073d8d7e
Michael <zxcvbnmike15@gmail.com>
2020-01-30 11:09:08 -0500

    Added Interpolation class  to other methods
    Added interpolation class calls to the standard process calling methods

--- hyo2/openbst/lib/processing/parameters.py
+++ hyo2/openbst/lib/processing/parameters.py
@@ -1,6 +1,8 @@
 import logging
 
 from hyo2.openbst.lib.processing.process_methods.dicts import ProcessMethods
+
+from hyo2.openbst.lib.processing.process_methods.interpolation import InterpParameters
 from hyo2.openbst.lib.processing.process_methods.raw_decoding import RawDecodeParameters
 from hyo2.openbst.lib.processing.process_methods.static_gain_compensation import StaticGainParameters
 from hyo2.openbst.lib.processing.process_methods.source_level import SourceLevelParameters
@@ -16,11 +18,14 @@ class Parameters:
         self.static_gains = StaticGainParameters()
         self.source_level = SourceLevelParameters()
         self.tvg = TVGCorrectionParameters()
+        self.interpolation = InterpParameters()
 
     # TODO: Can there be a check here to create object if not yet created
     def get_process_params(self, process_type: ProcessMethods):
         if process_type == ProcessMethods.RAWDECODE:
             return self.rawdecode
+        elif process_type == ProcessMethods.INTERPOLATION:
+            return self.interpolation
         elif process_type == ProcessMethods.STATICGAIN:
             return self.static_gains
         elif process_type == ProcessMethods.SOURCELEVEL:
