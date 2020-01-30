c37f981e7645a84cf306b4bb5fac00aebcba0b27
Michael <zxcvbnmike15@gmail.com>
2020-01-30 16:57:47 -0500

    Commented out code
    Some code that should have been commented out was not.

--- examples/workground/reson_smith/test_interp.py
+++ examples/workground/reson_smith/test_interp.py
@@ -25,38 +25,38 @@ bst.parameters.interpolation.method_type = InterpEnum.simple_linear
 bst.interpolation()
 
 # Inspect Data
-path_hash = bst.raws.raws_list[0]
-raw_file_path = bst.raws_folder.joinpath(path_hash + bst.raws.ext)
-process_file_path = bst.process_folder.joinpath(path_hash + bst.process.ext)
-
-ds_raw = Dataset(filename=raw_file_path, mode='r')
-ds_process = Dataset(filename=process_file_path, mode='r')
-grp_parent = ds_process.groups[bst.process.proc_manager.parent_process]
-
-# Grab raw data for comparison
-grp_position = ds_raw.groups['position']
-
-var_pos_time = grp_position.variables['time']
-var_latitude = grp_position.variables['latitude']
-var_longitude = grp_position.variables['longitude']
-data_pos_time = var_pos_time[:]
-data_latitude = var_latitude[:]
-data_longitude = var_longitude[:]
-
-grp_attitude = ds_raw.groups['attitude']
-
-var_motion_time = grp_attitude.variables['time']
-var_pitch = grp_attitude.variables['pitch']
-var_roll = grp_attitude.variables['roll']
-var_heave = grp_attitude.variables['heave']
-var_heading_time = grp_attitude.variables['heading_time']
-var_heading = grp_attitude.variables['heading']
-data_motion_time = var_motion_time[:]
-data_pitch = var_pitch[:]
-data_roll = var_roll[:]
-data_heave = var_heave[:]
-data_heading_time = var_heading_time[:]
-data_heading = var_heading[:]
+# path_hash = bst.raws.raws_list[0]
+# raw_file_path = bst.raws_folder.joinpath(path_hash + bst.raws.ext)
+# process_file_path = bst.process_folder.joinpath(path_hash + bst.process.ext)
+#
+# ds_raw = Dataset(filename=raw_file_path, mode='r')
+# ds_process = Dataset(filename=process_file_path, mode='r')
+# grp_parent = ds_process.groups[bst.process.proc_manager.parent_process]
+#
+# # Grab raw data for comparison
+# grp_position = ds_raw.groups['position']
+#
+# var_pos_time = grp_position.variables['time']
+# var_latitude = grp_position.variables['latitude']
+# var_longitude = grp_position.variables['longitude']
+# data_pos_time = var_pos_time[:]
+# data_latitude = var_latitude[:]
+# data_longitude = var_longitude[:]
+# 
+# grp_attitude = ds_raw.groups['attitude']
+#
+# var_motion_time = grp_attitude.variables['time']
+# var_pitch = grp_attitude.variables['pitch']
+# var_roll = grp_attitude.variables['roll']
+# var_heave = grp_attitude.variables['heave']
+# var_heading_time = grp_attitude.variables['heading_time']
+# var_heading = grp_attitude.variables['heading']
+# data_motion_time = var_motion_time[:]
+# data_pitch = var_pitch[:]
+# data_roll = var_roll[:]
+# data_heave = var_heave[:]
+# data_heading_time = var_heading_time[:]
+# data_heading = var_heading[:]
 
 # # Grab process data
 # var_time = grp_parent.variables['time']
