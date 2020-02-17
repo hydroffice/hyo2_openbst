331b319d7bbf8721f35b63592f774fc33d10b186
Michael <zxcvbnmike15@gmail.com>
2020-02-17 17:53:08 -0500

    Added methods to raytrace class

--- hyo2/openbst/lib/processing/process_methods/raytracing.py
+++ hyo2/openbst/lib/processing/process_methods/raytracing.py
@@ -48,6 +48,7 @@ class RayTraceParams:
         process_ids = [process_string, parameter_hash]
         return process_ids
 
+
 class RayTrace:
 
     def __init__(self):
@@ -61,22 +62,59 @@ class RayTrace:
         grp_runtime = ds_raw.groups['runtime_settings']
         var_sensor_soundspeed = grp_runtime.variables['sound_velocity']
         data_sensor_soundspeed = var_sensor_soundspeed[:]
+        data_sensor_soundspeed = data_sensor_soundspeed[:, np.newaxis]
+
+        grp_bathy = ds_raw.groups['raw_bathymetry_data']
+        var_sample_rate = grp_bathy.variables['sample_rate']
+        data_sample_rate = var_sample_rate[:]
+        data_sample_rate = data_sample_rate[:, np.newaxis]
+        var_detect_point = grp_bathy.variables['detect_point']
+        data_detect_point = var_detect_point[:]
 
         if p_method_type is RayTraceEnum.slant_range_approximation:
-            pass
+            # TODO: Assuming that sometime there is no surface ss probe, what do we use and how do we check
+            data_out = cls.slant_range(detection_point=data_detect_point,
+                                       surface_sound_speed=data_sensor_soundspeed,
+                                       sample_rate=data_sample_rate)
+
         elif p_method_type is RayTraceEnum.using_ssp:
-            pass
+            # TODO: Figure out a way to reference the motion compensation so I can actually do ssp calculations
+            grp_motion = ds_process.groups['PLACEHOLDER']
+            data_out = None
         else:
             raise TypeError("Unrecognized Raytrace Method Type: %s" % p_method_type)
         return data_out
 
     @classmethod
     def write_data_to_nc(cls,data_dict: dict, grp_process: Group):
-        pass
+        try:
+            for data_name, data in data_dict.items():
+                if data_name == 'range':
+                    grp_process.createDimension(dimname='ping', size=None)
+                    grp_process.createDimension(dimname='beam', size=None)
+                    var_range_data = grp_process.createVariable(varname='range',
+                                                             datatype='f8',
+                                                             dimensions=('ping', 'beam'))
+                    var_range_data.longname = "computed_range_to_seafloor"
+                    var_range_data.units = 'm'
+                    var_range_data[:] = data
+
+            return True
+        except RuntimeError:
+            return False
 
     @classmethod
-    def slant_range(cls):
-        pass
+    def slant_range(cls,
+                    detection_point: np.ma.MaskedArray,
+                    surface_sound_speed: np.ma.MaskedArray,
+                    sample_rate: np.ma.MaskedArray):
+
+        slant_range = detection_point / sample_rate * surface_sound_speed / 2
+
+        data_out = {
+            'range': slant_range
+        }
+        return data_out
 
     @classmethod
     def using_ssp(cls):
